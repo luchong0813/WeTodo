9233bf8a4ed07016dc4ec9e470377423f578289f
chong.lu <luchong1999@outlook.com>
2021-09-12 22:35:22 +0800

    首页待办与备忘列表

---
+++ src/WeTodoForWindows/Models/BaseDto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeTodoForWindows.Models
+{
+    /// <summary>
+    /// 基类DTO实体
+    /// </summary>
+    public class BaseDto
+    {
+        private int id;
+        private DateTime creatDate;
+        private DateTime updateDate;
+
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreatDate
+        {
+            get { return creatDate; }
+            set { creatDate = value; }
+        }
+
+        /// <summary>
+        /// 更细时间
+        /// </summary>
+        public DateTime UpdateDate
+        {
+            get { return updateDate; }
+            set { updateDate = value; }
+        }
+
+    }
+}
--- src/WeTodoForWindows/ViewModels/HomeViewModel.cs
+++ src/WeTodoForWindows/ViewModels/HomeViewModel.cs
@@ -17,14 +17,28 @@ namespace WeTodoForWindows.ViewModels
         {
             TaskBars = new ObservableCollection<TaskBar>();
             CreateTaskBars();
+            CraetTestDtoData();
         }
 
         private ObservableCollection<TaskBar> taskBars;
-
         public ObservableCollection<TaskBar> TaskBars
         {
             get { return taskBars; }
             set { taskBars = value; RaisePropertyChanged(); }
+        }
+
+        private ObservableCollection<TodoDto> todoDtos;
+        public ObservableCollection<TodoDto> TodoDtos
+        {
+            get { return todoDtos; }
+            set { todoDtos = value; RaisePropertyChanged(); }
+        }
+
+        private ObservableCollection<MemoDto> memoDtos;
+        public ObservableCollection<MemoDto> MemoDtos
+        {
+            get { return memoDtos; }
+            set { memoDtos = value; RaisePropertyChanged(); }
         }
 
         private void CreateTaskBars()
@@ -33,6 +47,17 @@ namespace WeTodoForWindows.ViewModels
             TaskBars.Add(new TaskBar { Icon = "ClockCheckOutline", Title = "已完成", Content = "45",  Color = "#10B138", Target = "" });
             TaskBars.Add(new TaskBar { Icon = "ChartLineVariant", Title = "完成比例", Content = "23",  Color = "#0097FF", Target = "" });
             TaskBars.Add(new TaskBar { Icon = "PlaylistStar", Title = "备忘录", Content = "5", Color = "#FFA000", Target = "" });
+        }
+
+        private void CraetTestDtoData()
+        {
+            TodoDtos = new ObservableCollection<TodoDto>();
+            MemoDtos = new ObservableCollection<MemoDto>();
+            for (int i = 0; i < 10; i++)
+            {
+                TodoDtos.Add(new TodoDto { Title = "待办" + i, Content = "Do Somthing..." });
+                MemoDtos.Add(new MemoDto { Title = "备忘" + i, Content = "Do Somthing..." });
+            }
         }
     }
 }
